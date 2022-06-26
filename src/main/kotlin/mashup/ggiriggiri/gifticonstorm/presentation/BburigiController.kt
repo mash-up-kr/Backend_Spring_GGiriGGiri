34f9e3d3b7c7d0199eb8b6d030070d146a60b7fe
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-06-26 21:57:21 +0900

    refactor: 뿌리기 컨트롤러 오타 수정

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/BburigiController.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/BburigiController.kt
@@ -1,7 +1,9 @@
 package mashup.ggiriggiri.gifticonstorm.presentation
 
 import mashup.ggiriggiri.gifticonstorm.domain.dto.event.CreateEventRequestDto
+import mashup.ggiriggiri.gifticonstorm.infrastructure.CouponImageUploader
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
@@ -9,16 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
 @RestController
-class BurrigiController {
+class BburigiController {
 
-    val logger = LoggerFactory.getLogger(javaClass)
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @PostMapping("/bburigi")
-    fun createBurrigi(
+    fun createBburigi(
         @RequestPart(value = "image") image: MultipartFile,
         @RequestPart(value = "eventInfo") dto: CreateEventRequestDto
     ) : ResponseEntity<Unit> {
-
         return ResponseEntity.ok(Unit);
     }
 }
