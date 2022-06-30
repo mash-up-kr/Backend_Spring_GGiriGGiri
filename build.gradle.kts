3567a05783cd4779393a41cbec8fdee8928c2dcb
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-07-01 01:23:09 +0900

    config: downgrade jdk version to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "mash-up.ggiriggiri"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_13
+java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
 	mavenCentral()
@@ -67,7 +67,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "13"
+		jvmTarget = "11"
 	}
 }
 
