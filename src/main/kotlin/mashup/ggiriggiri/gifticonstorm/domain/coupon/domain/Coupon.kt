afd56426e1df60abd9ab96a0a28971219c83487a
sw-develop <psw00719@naver.com>
2022-07-07 22:58:47 +0900

    chore: 패키지 이동

---
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/coupon/domain/Coupon.kt
@@ -0,0 +1,41 @@
+package mashup.ggiriggiri.gifticonstorm.domain.coupon.domain
+
+import mashup.ggiriggiri.gifticonstorm.domain.BaseEntity
+import mashup.ggiriggiri.gifticonstorm.domain.coupon.dto.CouponSaveRequestDto
+import mashup.ggiriggiri.gifticonstorm.domain.participant.Participant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.OneToMany
+
+@Entity
+class Coupon (
+    val brandName: String,
+    val merchandiseName: String,
+    val expiredAt: LocalDateTime,
+    val bburigiAt: LocalDateTime,
+    val imageUrl: String,
+
+    @Enumerated(EnumType.STRING)
+    val category: Category,
+
+    @OneToMany(mappedBy = "coupon")
+    val participants: MutableList<Participant> = mutableListOf()
+): BaseEntity() {
+
+    companion object {
+        fun of(imageUrl: String, couponSaveRequestDto: CouponSaveRequestDto): Coupon {
+            return Coupon(
+                brandName = couponSaveRequestDto.brandName,
+                merchandiseName = couponSaveRequestDto.merchandiseName,
+                expiredAt = LocalDate.parse(couponSaveRequestDto.couponExpiredTime).atTime(LocalTime.MAX),
+                bburigiAt = LocalDateTime.now().plusHours(couponSaveRequestDto.bburigiTime),
+                imageUrl = imageUrl,
+                category = couponSaveRequestDto.category
+            )
+        }
+    }
+}
