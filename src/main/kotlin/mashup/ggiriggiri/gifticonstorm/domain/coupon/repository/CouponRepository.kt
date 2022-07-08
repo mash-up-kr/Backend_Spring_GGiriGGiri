d46207ca83403b56ee3f9b9b8ee44f73b0fc923d
sw-develop <psw00719@naver.com>
2022-07-08 16:32:51 +0900

    chore: {}, stream 호출 제거

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/coupon/repository/CouponRepository.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/coupon/repository/CouponRepository.kt
@@ -3,5 +3,4 @@ package mashup.ggiriggiri.gifticonstorm.domain.coupon.repository
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Coupon
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface CouponRepository: JpaRepository<Coupon, Long> {
-}
+interface CouponRepository: JpaRepository<Coupon, Long>
