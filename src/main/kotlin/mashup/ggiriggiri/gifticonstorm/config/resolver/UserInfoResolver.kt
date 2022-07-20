3957409bd0a0188ba3439ba26e863a38fc63fe9e
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-07-21 01:00:08 +0900

    feat: UserResolver 로직 정의

---
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/config/resolver/UserInfoResolver.kt
@@ -0,0 +1,39 @@
+package mashup.ggiriggiri.gifticonstorm.config.resolver
+
+import mashup.ggiriggiri.gifticonstorm.common.error.exception.UnauthorizedException
+import mashup.ggiriggiri.gifticonstorm.config.annotation.UserInfo
+import mashup.ggiriggiri.gifticonstorm.domain.member.domain.Member
+import mashup.ggiriggiri.gifticonstorm.domain.member.repository.MemberRepository
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.bind.support.WebDataBinderFactory
+import org.springframework.web.context.request.NativeWebRequest
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.method.support.ModelAndViewContainer
+
+@Component
+class UserInfoResolver(private val memberRepository: MemberRepository) : HandlerMethodArgumentResolver {
+    companion object {
+        const val AUTHORIZATION_KEY_HEADER_NAME = "Authorization"
+    }
+
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(UserInfo::class.java) && parameter.parameter.type == UserInfoDto::class.java
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        mavContainer: ModelAndViewContainer?,
+        webRequest: NativeWebRequest,
+        binderFactory: WebDataBinderFactory?
+    ): Any? {
+        val authkey = webRequest.getHeader(AUTHORIZATION_KEY_HEADER_NAME) ?: throw UnauthorizedException()
+        val member = memberRepository.findByInherenceId(authkey) ?: memberRepository.save(Member(inherenceId =  authkey))
+        return UserInfoDto(id = member.id, inherenceId = member.inherenceId)
+    }
+}
+
+data class UserInfoDto(
+    val id: Long,
+    val inherenceId: String
+)
---
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/member/repository/MemberRespository.kt
@@ -0,0 +1,8 @@
+package mashup.ggiriggiri.gifticonstorm.domain.member.repository
+
+import mashup.ggiriggiri.gifticonstorm.domain.member.domain.Member
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface MemberRepository : JpaRepository<Member, Long> {
+    fun findByInherenceId(inherenceId: String): Member?
+}
