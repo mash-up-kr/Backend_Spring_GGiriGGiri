37d3934a072a182b83401044be59cc70acfd8308
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-07-21 02:15:45 +0900

    chore: bean order 수정

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/config/WebMvcConfig.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/config/WebMvcConfig.kt
@@ -1,12 +1,17 @@
 package mashup.ggiriggiri.gifticonstorm.config
 
 import mashup.ggiriggiri.gifticonstorm.config.resolver.UserInfoResolver
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
+@Order(10)
 class WebMvcConfig(private val userInfoResolver: UserInfoResolver) : WebMvcConfigurer {
+
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(userInfoResolver)
     }
--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/member/repository/MemberRepository.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/member/repository/MemberRepository.kt
@@ -1,8 +1,11 @@
 package mashup.ggiriggiri.gifticonstorm.domain.member.repository
 
 import mashup.ggiriggiri.gifticonstorm.domain.member.domain.Member
+import org.springframework.boot.autoconfigure.AutoConfigureOrder
+import org.springframework.core.annotation.Order
 import org.springframework.data.jpa.repository.JpaRepository
 
+@Order(0)
 interface MemberRepository : JpaRepository<Member, Long> {
     fun findByInherenceId(inherenceId: String): Member?
 }
