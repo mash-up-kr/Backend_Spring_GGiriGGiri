546eeb05d8fc7c7bbaacde2dd76b4420586fe429
seongchan.hwang <seongchan.hwang@navercorp.com>
2022-07-31 15:02:03 +0900

    refactor: Ocr Result 포맷팅

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/infrastructure/OcrResult.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/infrastructure/OcrResult.kt
@@ -3,22 +3,22 @@ package mashup.ggiriggiri.gifticonstorm.infrastructure
 // TODO: 카카오 쿠폰 등등 종류별로 사용할 수 있도록 객체 관계 설계
 
 interface OcrResult {
-    fun isSuccess() : Boolean
-    fun toDto() : OcrResultResponseDto
+    fun isSuccess(): Boolean
+    fun toDto(): OcrResultResponseDto
 }
 
 data class NaverOcrResult(
     val images: List<Images>,
 ) : OcrResult {
 
-    override fun isSuccess() : Boolean {
+    override fun isSuccess(): Boolean {
         return images[0].isSuccess()
     }
 
     override fun toDto() = OcrResultResponseDto(
-                images[0].fields.first { it.name == "brandName" }.inferText,
-                images[0].fields.first { it.name == "merchandiseName" }.inferText,
-                images[0].fields.first { it.name == "expiredAt" }.inferText
+        _brandName = images[0].fields.first { it.name == "brandName" }.inferText,
+        _merchandiseName = images[0].fields.first { it.name == "merchandiseName" }.inferText,
+        _expiredAt = images[0].fields.first { it.name == "expiredAt" }.inferText
     )
 
 }
