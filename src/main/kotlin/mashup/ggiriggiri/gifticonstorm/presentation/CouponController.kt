5d2ae52cff448a748662252ca10f97a801bf873d
sw-develop <psw00719@naver.com>
2022-08-03 20:28:35 +0900

    add: 쿠폰 카테고리 목록 조회

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/CouponController.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/CouponController.kt
@@ -2,7 +2,9 @@ package mashup.ggiriggiri.gifticonstorm.presentation
 
 import mashup.ggiriggiri.gifticonstorm.application.CouponService
 import mashup.ggiriggiri.gifticonstorm.common.dto.BaseResponse
+import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Category
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.dto.CouponSaveRequestDto
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
@@ -20,5 +22,10 @@ class CouponController(
         @RequestPart(value = "couponInfo") @Valid requestDto: CouponSaveRequestDto
     ): BaseResponse<Unit> {
         return BaseResponse.ok()
+    }
+
+    @GetMapping("/api/v1/coupon/category")
+    fun getCategory(): BaseResponse<Array<Category>> {
+        return BaseResponse.ok(Category.categories)
     }
 }
