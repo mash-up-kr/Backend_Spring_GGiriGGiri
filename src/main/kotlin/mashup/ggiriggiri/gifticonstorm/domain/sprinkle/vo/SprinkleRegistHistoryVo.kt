4d9d754cae47d138c249eb74830ffc6c41cb9d45
sw-develop <psw00719@naver.com>
2022-08-05 00:29:57 +0900

    add: 뿌리기 등록 내역 조회

---
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/vo/SprinkleRegistHistoryVo.kt
@@ -0,0 +1,15 @@
+package mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo
+
+import com.querydsl.core.annotations.QueryProjection
+import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Category
+import java.time.LocalDateTime
+
+data class SprinkleRegistHistoryVo @QueryProjection constructor(
+    val brandName: String,
+    val merchandiseName: String,
+    val expiredAt: LocalDateTime,
+    val category: Category,
+    val participants: Int,
+    val sprinkled: Boolean,
+    val sprinkleAt: LocalDateTime
+)
