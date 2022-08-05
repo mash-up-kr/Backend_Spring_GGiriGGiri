d564ec4865b8e450a8aaf47105b0c62d08f62bb9
sw-develop <psw00719@naver.com>
2022-08-05 18:33:22 +0900

    add: 응모 내역 조회 기능

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/participant/repository/ParticipantRepositoryCustom.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/participant/repository/ParticipantRepositoryCustom.kt
@@ -1,6 +1,11 @@
 package mashup.ggiriggiri.gifticonstorm.domain.participant.repository
 
+import mashup.ggiriggiri.gifticonstorm.common.dto.NoOffsetRequest
+import mashup.ggiriggiri.gifticonstorm.domain.participant.vo.ParticipantInfoVo
+
 interface ParticipantRepositoryCustom {
 
     fun findAllSprinkleIdByMemberId(memberId: Long): List<Long>
+
+    fun findHistoryByMemberId(memberId: Long, noOffsetRequest: NoOffsetRequest): List<ParticipantInfoVo>
 }
--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/participant/repository/ParticipantRepositoryCustomImpl.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/participant/repository/ParticipantRepositoryCustomImpl.kt
@@ -1,7 +1,13 @@
 package mashup.ggiriggiri.gifticonstorm.domain.participant.repository
 
+import com.querydsl.core.types.dsl.BooleanExpression
 import com.querydsl.jpa.impl.JPAQueryFactory
+import mashup.ggiriggiri.gifticonstorm.common.dto.NoOffsetRequest
+import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.QCoupon.coupon
 import mashup.ggiriggiri.gifticonstorm.domain.participant.QParticipant.participant
+import mashup.ggiriggiri.gifticonstorm.domain.participant.vo.ParticipantInfoVo
+import mashup.ggiriggiri.gifticonstorm.domain.participant.vo.QParticipantInfoVo
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.domain.QSprinkle.sprinkle
 
 class ParticipantRepositoryCustomImpl(
     private val jpaQueryFactory: JPAQueryFactory
@@ -13,5 +19,35 @@ class ParticipantRepositoryCustomImpl(
             .from(participant)
             .where(participant.member.id.eq(memberId))
             .fetch()
+    }
+
+    override fun findHistoryByMemberId(memberId: Long, noOffsetRequest: NoOffsetRequest): List<ParticipantInfoVo> {
+        return jpaQueryFactory
+            .select(QParticipantInfoVo(
+                participant.id,
+                sprinkle.id,
+                coupon.brandName,
+                coupon.merchandiseName,
+                coupon.expiredAt,
+                coupon.category,
+                sprinkle.participants.size(),
+                participant.createdAt,
+                participant.isChecked,
+                participant.drawStatus
+            ))
+            .from(participant)
+            .join(participant.sprinkle, sprinkle)
+            .join(sprinkle.coupon, coupon)
+            .where(
+                ltParticipantId(noOffsetRequest.id),
+                participant.member.id.eq(memberId)
+            )
+            .orderBy(participant.id.desc())
+            .limit(noOffsetRequest.limit)
+            .fetch()
+    }
+
+    private fun ltParticipantId(id: Long?): BooleanExpression? {
+        return id?.let { participant.id.lt(id) }
     }
 }
