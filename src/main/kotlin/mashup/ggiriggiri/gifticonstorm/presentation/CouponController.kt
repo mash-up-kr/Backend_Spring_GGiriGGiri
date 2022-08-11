bd1763b76a117b60ba599e1817324880b8507cf1
luffy <whdgus8219@naver.com>
2022-08-11 20:48:41 +0900

    불필요 코드 제거

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/CouponController.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/CouponController.kt
@@ -1,20 +1,12 @@
 package mashup.ggiriggiri.gifticonstorm.presentation
 
-import mashup.ggiriggiri.gifticonstorm.application.CouponService
 import mashup.ggiriggiri.gifticonstorm.common.dto.BaseResponse
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Category
-import mashup.ggiriggiri.gifticonstorm.domain.coupon.dto.CouponSaveRequestDto
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestPart
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.multipart.MultipartFile
-import javax.validation.Valid
 
 @RestController
-class CouponController(
-    private val couponService: CouponService
-) {
+class CouponController {
 
     @GetMapping("/api/v1/coupon/category")
     fun getCategory(): BaseResponse<Array<Category>> {
