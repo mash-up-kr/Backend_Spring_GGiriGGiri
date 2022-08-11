fdfe423460d87ae9ef45cf69731078633d7aad59
luffy <whdgus8219@naver.com>
2022-08-11 21:23:49 +0900

    응모 결과 조회 mock api 추가

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/SprinkleController.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/presentation/SprinkleController.kt
@@ -1,5 +1,6 @@
 package mashup.ggiriggiri.gifticonstorm.presentation
 
+import mashup.ggiriggiri.gifticonstorm.application.push.DrawStatus
 import mashup.ggiriggiri.gifticonstorm.application.sprinkle.SprinkleService
 import mashup.ggiriggiri.gifticonstorm.common.dto.BaseResponse
 import mashup.ggiriggiri.gifticonstorm.common.dto.NoOffsetRequest
@@ -8,11 +9,15 @@ import mashup.ggiriggiri.gifticonstorm.config.resolver.UserInfoDto
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Category
 import mashup.ggiriggiri.gifticonstorm.domain.dto.event.CreateEventRequestDto
 import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.domain.OrderBy
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.dto.CouponInfoDto
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.dto.DrawResultResDto
 import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.dto.GetSprinkleResDto
 import mashup.ggiriggiri.gifticonstorm.infrastructure.Logger
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDateTime
 import javax.validation.Valid
+import kotlin.random.Random
 
 @RestController
 @RequestMapping("/api/v1")
@@ -46,5 +51,13 @@ class SprinkleController(
         noOffsetRequest: NoOffsetRequest,
     ): BaseResponse<List<GetSprinkleResDto>> {
         return BaseResponse.ok(sprinkleService.getSprinkles(userInfoDto, orderBy, category, noOffsetRequest))
+    }
+
+    @GetMapping("/draw-result/{sprinkleId}")
+    fun getDrawResult(@UserInfo userInfoDto: UserInfoDto, @PathVariable sprinkleId: Long): DrawResultResDto {
+        return if (Random.nextBoolean())
+            DrawResultResDto(DrawStatus.WIN, CouponInfoDto("베스킨라빈스", "싱글 레귤러", LocalDateTime.now().plusMonths(2), "http://dummyimage.com/240x100.png/dddddd/000000"))
+        else
+            DrawResultResDto(DrawStatus.LOSE)
     }
 }
