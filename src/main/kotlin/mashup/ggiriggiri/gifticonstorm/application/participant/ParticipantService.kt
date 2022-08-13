cebc749001bbe6edc9ad76c96ceaca9d3eb81e0c
luffy <whdgus8219@naver.com>
2022-08-13 22:05:50 +0900

    응모 결과 api 추가

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/application/participant/ParticipantService.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/application/participant/ParticipantService.kt
@@ -1,19 +1,34 @@
 package mashup.ggiriggiri.gifticonstorm.application.participant
 
 import mashup.ggiriggiri.gifticonstorm.common.dto.NoOffsetRequest
+import mashup.ggiriggiri.gifticonstorm.common.dto.ResponseCode
+import mashup.ggiriggiri.gifticonstorm.common.error.exception.BaseException
 import mashup.ggiriggiri.gifticonstorm.config.resolver.UserInfoDto
 import mashup.ggiriggiri.gifticonstorm.domain.participant.dto.ParticipantInfoResDto
 import mashup.ggiriggiri.gifticonstorm.domain.participant.repository.ParticipantRepository
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.dto.DrawResultResDto
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.repository.SprinkleRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
 class ParticipantService(
-    private val participantRepository: ParticipantRepository
+    private val participantRepository: ParticipantRepository,
+    private val sprinkleRepository: SprinkleRepository
 ) {
 
     fun getParticipantHistory(userInfoDto: UserInfoDto, noOffsetRequest: NoOffsetRequest): List<ParticipantInfoResDto> {
         val participantInfoVos = participantRepository.findHistoryByMemberId(userInfoDto.id, noOffsetRequest)
         return participantInfoVos.map { ParticipantInfoResDto.of(it) }
+    }
+
+    fun getDrawResult(userInfo: UserInfoDto, sprinkleId: Long): DrawResultResDto {
+        val sprinkle = sprinkleRepository.findByIdOrNull(sprinkleId) ?: throw BaseException(ResponseCode.DATA_NOT_FOUND, "sprinkle not found -> sprinkleId : $sprinkleId")
+        val participant = sprinkle.participants.firstOrNull { participant ->
+            participant.member.id == userInfo.id
+        } ?: throw BaseException(ResponseCode.DATA_NOT_FOUND, "참여하지 않은 뿌리기 -> memberId : ${userInfo.id} sprinkleId : $sprinkleId")
+
+        return DrawResultResDto.of(participant.drawStatus, sprinkle.coupon)
     }
 
 }
--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/dto/DrawResultResDto.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/dto/DrawResultResDto.kt
@@ -2,17 +2,38 @@ package mashup.ggiriggiri.gifticonstorm.domain.sprinkle.dto
 
 import com.fasterxml.jackson.annotation.JsonInclude
 import mashup.ggiriggiri.gifticonstorm.application.push.DrawStatus
+import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Coupon
 import java.time.LocalDateTime
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 data class DrawResultResDto(
     val drawStatus: DrawStatus,
     val couponInfo: CouponInfoDto? = null
-)
+) {
+    companion object {
+        fun of(drawStatus: DrawStatus, coupon: Coupon): DrawResultResDto {
+            return if (drawStatus == DrawStatus.WIN)
+                DrawResultResDto(drawStatus, CouponInfoDto.of(coupon))
+            else
+                DrawResultResDto(drawStatus)
+        }
+    }
+}
 
 data class CouponInfoDto(
     val brandName: String,
     val merchandiseName: String,
     val expiredAt: LocalDateTime,
     val imageUrl: String
-)
+) {
+    companion object {
+        fun of(coupon: Coupon): CouponInfoDto {
+            return CouponInfoDto(
+                brandName = coupon.brandName,
+                merchandiseName = coupon.merchandiseName,
+                expiredAt = coupon.expiredAt,
+                imageUrl = coupon.imageUrl
+            )
+        }
+    }
+}
