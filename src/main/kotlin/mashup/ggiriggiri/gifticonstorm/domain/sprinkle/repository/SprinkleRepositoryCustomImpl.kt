bbd13b40ffabc54cfbc563212dfdb92f85a8ac36
sw-develop <psw00719@naver.com>
2022-08-15 21:09:04 +0900

    chore: 뿌리기 조회용 Vo 클래스 분리

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/repository/SprinkleRepositoryCustomImpl.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/repository/SprinkleRepositoryCustomImpl.kt
@@ -6,10 +6,7 @@ import mashup.ggiriggiri.gifticonstorm.common.dto.NoOffsetRequest
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.Category
 import mashup.ggiriggiri.gifticonstorm.domain.coupon.domain.QCoupon.coupon
 import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.domain.QSprinkle.sprinkle
-import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo.QSprinkleInfoVo
-import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo.QSprinkleRegistHistoryVo
-import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo.SprinkleInfoVo
-import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo.SprinkleRegistHistoryVo
+import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.vo.*
 import java.time.LocalDateTime
 import java.util.stream.Collectors
 
@@ -18,37 +15,35 @@ class SprinkleRepositoryCustomImpl(
 ) : SprinkleRepositoryCustom {
 
     //뿌리기 남은 시간 {leftMinute} 이내 & 참여자 수 많은 것 중 상위 {limit}개
-    override fun findAllByDeadLine(leftMinute: Long, limit: Long): List<SprinkleInfoVo> {
+    override fun findAllByDeadLine(leftMinute: Long, limit: Long): List<GetSprinkleVo> {
         return jpaQueryFactory
-            .select(QSprinkleInfoVo(
+            .select(QGetSprinkleVo(
                 sprinkle.id,
                 coupon.brandName,
                 coupon.merchandiseName,
                 coupon.category,
                 coupon.expiredAt,
                 sprinkle.participants.size(),
-                sprinkle.sprinkleAt,
-                sprinkle.member.id
+                sprinkle.sprinkleAt
             ))
             .from(sprinkle)
             .join(sprinkle.coupon, coupon)
             .where(withinTime(leftMinute))
             .fetch().stream().sorted(
-                Comparator.comparingInt(SprinkleInfoVo::participants).reversed()
+                Comparator.comparingInt(GetSprinkleVo::participants).reversed()
             ).limit(limit).collect(Collectors.toList())
     }
 
-    override fun findAllByCategory(category: Category, noOffsetRequest: NoOffsetRequest): List<SprinkleInfoVo> {
+    override fun findAllByCategory(category: Category, noOffsetRequest: NoOffsetRequest): List<GetSprinkleVo> {
         return jpaQueryFactory
-            .select(QSprinkleInfoVo(
+            .select(QGetSprinkleVo(
                 sprinkle.id,
                 coupon.brandName,
                 coupon.merchandiseName,
                 coupon.category,
                 coupon.expiredAt,
                 sprinkle.participants.size(),
-                sprinkle.sprinkleAt,
-                sprinkle.member.id
+                sprinkle.sprinkleAt
             ))
             .from(sprinkle)
             .join(sprinkle.coupon, coupon)
@@ -70,7 +65,6 @@ class SprinkleRepositoryCustomImpl(
                 coupon.merchandiseName,
                 coupon.category,
                 coupon.expiredAt,
-                sprinkle.participants.size(),
                 sprinkle.sprinkleAt,
                 sprinkle.member.id
             ))
