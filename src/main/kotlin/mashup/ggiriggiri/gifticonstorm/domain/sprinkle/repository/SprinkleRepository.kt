3efcd757f08293b59e11c4a08193402b18fe5e65
sw-develop <psw00719@naver.com>
2022-09-03 01:27:15 +0900

    fix: 뿌리기 대상 조회 쿼리 수정

--- src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/repository/SprinkleRepository.kt
+++ src/main/kotlin/mashup/ggiriggiri/gifticonstorm/domain/sprinkle/repository/SprinkleRepository.kt
@@ -2,7 +2,9 @@ package mashup.ggiriggiri.gifticonstorm.domain.sprinkle.repository
 
 import mashup.ggiriggiri.gifticonstorm.domain.sprinkle.domain.Sprinkle
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 
 interface SprinkleRepository: JpaRepository<Sprinkle, Long>, SprinkleRepositoryCustom {
-    fun findAllBySprinkledFalse() : List<Sprinkle>
+
+    fun findAllBySprinkledFalseAndSprinkleAtLessThanEqual(time: LocalDateTime): List<Sprinkle>
 }
